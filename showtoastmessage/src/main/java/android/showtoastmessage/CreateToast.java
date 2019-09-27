ab0d415714b32e97b5dd593847e213d3f7637efa
Purushotam <purushotamkr198@gmail.com>
2019-09-27 23:13:10 +0530

    added popToast method

--- showtoastmessage/src/main/java/android/showtoastmessage/CreateToast.java
+++ showtoastmessage/src/main/java/android/showtoastmessage/CreateToast.java
@@ -1,8 +1,12 @@
 package android.showtoastmessage;
 
-import android.widget.TextView;
+import android.content.Context;
+import android.widget.Toast;
 
 public class CreateToast {
 
+    public void PopToast(Context context, String toastMessage){
+        Toast.makeText(context,toastMessage,Toast.LENGTH_SHORT).show();
+    }
 
 }
